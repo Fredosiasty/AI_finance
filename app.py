97c8c0d94b5b4d16f8b4d0076b632bf0e8e7c312
Maksymilian Tomaszewski <91779582+Fredosiasty@users.noreply.github.com>
2024-08-20 16:49:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def predcition():
     return(render_template("predcition.html"))
 
 @app.route("/joke",methods=["GET","POST"])
-def predcition():
+def joke():
     return(render_template("joke.html"))
 
 if __name__ == "__main__":
