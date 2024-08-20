e6216a90093d1d931dfe2ecddd7882b39f536ee6
Maksymilian Tomaszewski <91779582+Fredosiasty@users.noreply.github.com>
2024-08-20 16:42:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,30 @@
-from flask import Flask, render_template, request, palm
+from flask import Flask,render_template,request
+import google.generativeai as palm
+import os
 
-
-api = "AIzaSyBcXAU-ZRyUPaW2joQOVmSqWXHW4awuhKY"
+api = os.getenv("MAKERSUITE_API_TOKEN") 
 palm.configure(api_key=api)
+model = {"model": "models/chat-bison-001"}
+
 app = Flask(__name__)
 
-@app.route('/index', methods=['GET', 'POST'])
+@app.route("/",methods=["GET","POST"])
 def index():
-    return render_template('index.html')
+    return(render_template("index.html"))
 
-@app.route('/financial_QA', methods=['GET', 'POST'])
+@app.route("/financial_QA",methods=["GET","POST"])
 def financial_QA():
-    return render_template('financial_QA.html')
+    return(render_template("financial_QA.html"))
 
-@app.route('/makersuite', methods=['GET', 'POST'])
+@app.route("/makersuite",methods=["GET","POST"])
 def makersuite():
     q = request.form.get("q")
     r = palm.chat(prompt=q, **model)
-    return render_template('makersuite.html', r=r.last)
+    return(render_template("makersuite.html",r=r.last))
 
-@app.route('/prediction', methods=['GET', 'POST'])
-def prediction():
-    return render_template('prediction.html')
+@app.route("/predcition",methods=["GET","POST"])
+def predcition():
+    return(render_template("predcition.html"))
 
-@app.route('/joke', methods=['GET', 'POST'])
-def prediction():
-    return render_template('joke.html')
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
